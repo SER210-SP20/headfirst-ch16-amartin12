39db90931267809b2261d40c692703fe4b1d11a9
Alexandra Martin <amartin1@quinnipiac.edu>
2020-04-07 15:30:31 -0400

    Chapter 16 SQl StarBuzz

---
+++ app/src/main/java/edu/quinnipiac/ser210/ch16starbuzz/TopLevelActivity.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.ch16starbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}
